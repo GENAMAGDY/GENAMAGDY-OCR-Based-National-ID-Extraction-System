911f6a315803db368a0923df0fa37015087dfdb1
GENAMAGDY <70176445+GENAMAGDY@users.noreply.github.com>
2024-10-02 15:11:08 +0300

    Add files via upload

---
+++ Data Entry/Data Entry Job/Admin.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Data_Entry_Job
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.open_browser = new System.Windows.Forms.Button();
+            this.Name_User = new System.Windows.Forms.Label();
+            this.Image1 = new System.Windows.Forms.PictureBox();
+            this.Image2 = new System.Windows.Forms.PictureBox();
+            this.Field1 = new System.Windows.Forms.PictureBox();
+            this.Field2 = new System.Windows.Forms.PictureBox();
+            this.Field3 = new System.Windows.Forms.PictureBox();
+            this.error = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.Delete_Field1 = new System.Windows.Forms.Button();
+            this.Add1 = new System.Windows.Forms.Button();
+            this.Add2 = new System.Windows.Forms.Button();
+            this.Delete_Field2 = new System.Windows.Forms.Button();
+            this.Field_1 = new System.Windows.Forms.Label();
+            this.Field_2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Image1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Image2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Mistral", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Maroon;
+            this.label8.Location = new System.Drawing.Point(12, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 33);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Logout";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // open_browser
+            // 
+            this.open_browser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.open_browser.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open_browser.ForeColor = System.Drawing.Color.White;
+            this.open_browser.Location = new System.Drawing.Point(12, 501);
+            this.open_browser.Name = "open_browser";
+            this.open_browser.Size = new System.Drawing.Size(153, 46);
+            this.open_browser.TabIndex = 20;
+            this.open_browser.Text = "Open Browser";
+            this.open_browser.UseVisualStyleBackColor = false;
+            this.open_browser.Click += new System.EventHandler(this.open_browser_Click);
+            // 
+            // Name_User
+            // 
+            this.Name_User.AutoSize = true;
+            this.Name_User.BackColor = System.Drawing.Color.Transparent;
+            this.Name_User.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name_User.ForeColor = System.Drawing.Color.White;
+            this.Name_User.Location = new System.Drawing.Point(342, 16);
+            this.Name_User.Name = "Name_User";
+            this.Name_User.Size = new System.Drawing.Size(144, 26);
+            this.Name_User.TabIndex = 23;
+            this.Name_User.Text = "Name_User";
+            // 
+            // Image1
+            // 
+            this.Image1.BackColor = System.Drawing.Color.Transparent;
+            this.Image1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Image1.Location = new System.Drawing.Point(55, 65);
+            this.Image1.Name = "Image1";
+            this.Image1.Size = new System.Drawing.Size(391, 195);
+            this.Image1.TabIndex = 29;
+            this.Image1.TabStop = false;
+            this.Image1.Click += new System.EventHandler(this.Image1_Click);
+            this.Image1.Paint += new System.Windows.Forms.PaintEventHandler(this.Image1_Paint);
+            this.Image1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Image1_MouseDown);
+            this.Image1.MouseEnter += new System.EventHandler(this.Image1_MouseEnter);
+            this.Image1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Image1_MouseMove);
+            this.Image1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Image1_MouseUp);
+            // 
+            // Image2
+            // 
+            this.Image2.BackColor = System.Drawing.Color.Transparent;
+            this.Image2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Image2.Location = new System.Drawing.Point(465, 65);
+            this.Image2.Name = "Image2";
+            this.Image2.Size = new System.Drawing.Size(397, 195);
+            this.Image2.TabIndex = 30;
+            this.Image2.TabStop = false;
+            this.Image2.Click += new System.EventHandler(this.Image2_Click);
+            this.Image2.Paint += new System.Windows.Forms.PaintEventHandler(this.Image2_Paint);
+            this.Image2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Image2_MouseDown);
+            this.Image2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Image2_MouseMove);
+            this.Image2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Image2_MouseUp);
+            // 
+            // Field1
+            // 
+            this.Field1.BackColor = System.Drawing.Color.Transparent;
+            this.Field1.Location = new System.Drawing.Point(28, 333);
+            this.Field1.Name = "Field1";
+            this.Field1.Size = new System.Drawing.Size(246, 126);
+            this.Field1.TabIndex = 31;
+            this.Field1.TabStop = false;
+            // 
+            // Field2
+            // 
+            this.Field2.BackColor = System.Drawing.Color.Transparent;
+            this.Field2.Location = new System.Drawing.Point(296, 333);
+            this.Field2.Name = "Field2";
+            this.Field2.Size = new System.Drawing.Size(244, 126);
+            this.Field2.TabIndex = 32;
+            this.Field2.TabStop = false;
+            // 
+            // Field3
+            // 
+            this.Field3.BackColor = System.Drawing.Color.Transparent;
+            this.Field3.Location = new System.Drawing.Point(593, 333);
+            this.Field3.Name = "Field3";
+            this.Field3.Size = new System.Drawing.Size(173, 142);
+            this.Field3.TabIndex = 33;
+            this.Field3.TabStop = false;
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.BackColor = System.Drawing.Color.Transparent;
+            this.error.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.error.ForeColor = System.Drawing.Color.Red;
+            this.error.Location = new System.Drawing.Point(205, 519);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(47, 20);
+            this.error.TabIndex = 34;
+            this.error.Text = "error";
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Save.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.ForeColor = System.Drawing.Color.White;
+            this.Save.Location = new System.Drawing.Point(734, 506);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(153, 46);
+            this.Save.TabIndex = 35;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Delete_Field1
+            // 
+            this.Delete_Field1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Delete_Field1.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_Field1.ForeColor = System.Drawing.Color.White;
+            this.Delete_Field1.Location = new System.Drawing.Point(242, 273);
+            this.Delete_Field1.Name = "Delete_Field1";
+            this.Delete_Field1.Size = new System.Drawing.Size(117, 46);
+            this.Delete_Field1.TabIndex = 36;
+            this.Delete_Field1.Text = "Delete Field";
+            this.Delete_Field1.UseVisualStyleBackColor = false;
+            this.Delete_Field1.Click += new System.EventHandler(this.Delete_Field1_Click);
+            // 
+            // Add1
+            // 
+            this.Add1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Add1.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add1.ForeColor = System.Drawing.Color.White;
+            this.Add1.Location = new System.Drawing.Point(108, 273);
+            this.Add1.Name = "Add1";
+            this.Add1.Size = new System.Drawing.Size(87, 46);
+            this.Add1.TabIndex = 37;
+            this.Add1.Text = "Add";
+            this.Add1.UseVisualStyleBackColor = false;
+            this.Add1.Click += new System.EventHandler(this.Add1_Click);
+            // 
+            // Add2
+            // 
+            this.Add2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Add2.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add2.ForeColor = System.Drawing.Color.White;
+            this.Add2.Location = new System.Drawing.Point(509, 273);
+            this.Add2.Name = "Add2";
+            this.Add2.Size = new System.Drawing.Size(87, 46);
+            this.Add2.TabIndex = 40;
+            this.Add2.Text = "Add";
+            this.Add2.UseVisualStyleBackColor = false;
+            this.Add2.Click += new System.EventHandler(this.Add2_Click);
+            // 
+            // Delete_Field2
+            // 
+            this.Delete_Field2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Delete_Field2.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_Field2.ForeColor = System.Drawing.Color.White;
+            this.Delete_Field2.Location = new System.Drawing.Point(629, 273);
+            this.Delete_Field2.Name = "Delete_Field2";
+            this.Delete_Field2.Size = new System.Drawing.Size(117, 46);
+            this.Delete_Field2.TabIndex = 39;
+            this.Delete_Field2.Text = "Delete Field";
+            this.Delete_Field2.UseVisualStyleBackColor = false;
+            this.Delete_Field2.Click += new System.EventHandler(this.Delete_Field2_Click);
+            // 
+            // Field_1
+            // 
+            this.Field_1.AutoSize = true;
+            this.Field_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Field_1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Field_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Field_1.Location = new System.Drawing.Point(375, 266);
+            this.Field_1.Name = "Field_1";
+            this.Field_1.Size = new System.Drawing.Size(72, 21);
+            this.Field_1.TabIndex = 41;
+            this.Field_1.Text = "Field 1";
+            this.Field_1.Click += new System.EventHandler(this.Field_1_Click);
+            // 
+            // Field_2
+            // 
+            this.Field_2.AutoSize = true;
+            this.Field_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Field_2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Field_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Field_2.Location = new System.Drawing.Point(375, 302);
+            this.Field_2.Name = "Field_2";
+            this.Field_2.Size = new System.Drawing.Size(72, 21);
+            this.Field_2.TabIndex = 42;
+            this.Field_2.Text = "Field 2";
+            this.Field_2.Click += new System.EventHandler(this.Field_2_Click);
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = global::Data_Entry_Job.Properties.Resources.Screen_Shot_2020_08_24_at_10_05_11_AM_1024x657;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(899, 559);
+            this.Controls.Add(this.Field_2);
+            this.Controls.Add(this.Field_1);
+            this.Controls.Add(this.Add2);
+            this.Controls.Add(this.Delete_Field2);
+            this.Controls.Add(this.Add1);
+            this.Controls.Add(this.Delete_Field1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.Field3);
+            this.Controls.Add(this.Field2);
+            this.Controls.Add(this.Field1);
+            this.Controls.Add(this.Image2);
+            this.Controls.Add(this.Image1);
+            this.Controls.Add(this.Name_User);
+            this.Controls.Add(this.open_browser);
+            this.Controls.Add(this.label8);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.Load += new System.EventHandler(this.Admin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Image1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Image2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button open_browser;
+        private System.Windows.Forms.Label Name_User;
+        private System.Windows.Forms.PictureBox Image1;
+        private System.Windows.Forms.PictureBox Image2;
+        private System.Windows.Forms.PictureBox Field1;
+        private System.Windows.Forms.PictureBox Field2;
+        private System.Windows.Forms.PictureBox Field3;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Delete_Field1;
+        private System.Windows.Forms.Button Add1;
+        private System.Windows.Forms.Button Add2;
+        private System.Windows.Forms.Button Delete_Field2;
+        private System.Windows.Forms.Label Field_1;
+        private System.Windows.Forms.Label Field_2;
+    }
+}
---
+++ Data Entry/Data Entry Job/Admin.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+namespace Data_Entry_Job
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+            error.Text = "";
+            Name_User.Text = "";
+            Image1.Visible = false;
+            Image2.Visible = false;
+            Field_1.Visible = false;
+            Field_2.Visible = false;
+            Add1.Visible = false;
+            Add2.Visible = false;
+            Delete_Field1.Visible = false;
+            Delete_Field2.Visible = false;
+
+        }
+        int cropX1, cropY1, recW1, recH1;
+        int cropX2, cropY2, recW2, recH2;
+
+        bool f1 = false;
+        bool f2 = false;
+
+        bool f3 = false;
+
+        public Pen crop_pen = new Pen(Color.Red);
+        private void open_browser_Click(object sender, EventArgs e)
+        {
+            error.Text = "";
+            OpenFileDialog browser = new System.Windows.Forms.OpenFileDialog();
+            string path = "";
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                Name_User.Text = browser.SafeFileName;
+                path = browser.FileName;
+            }
+            // Split Multi tiff 
+            List<Image> images = Helper_Functions.Split_Multitiff(path);
+            Image1.SizeMode = PictureBoxSizeMode.StretchImage;
+            Image2.SizeMode = PictureBoxSizeMode.StretchImage;
+            Image1.Image = images[0]; 
+            Image2.Image = images[1];
+
+            Image1.Visible = true;
+            Image2.Visible = true;
+            f1 = false;
+            f2 = false;
+            f3 = false;
+
+            if (Field1.Image != null || Field2.Image != null || Field3.Image != null)
+            {
+                Field1.Image = null;
+                Field2.Image = null;
+                Field3.Image = null;
+            }
+
+            Field_1.Visible = false;
+            Field_2.Visible = false;
+            Add1.Visible = true;
+            Add2.Visible = true;
+            Delete_Field1.Visible = true;
+            Delete_Field2.Visible = true;
+        }
+
+        private void Image1_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Cursor = Cursors.Cross;
+                crop_pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                cropX1 = e.X;
+                cropY1 = e.Y;
+
+            }
+
+        }
+
+        private void Image1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Image1_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Image1.Refresh();
+                recH1 = e.Y - cropY1;
+                recW1 = e.X - cropX1;
+                Graphics graph = Image1.CreateGraphics();
+                graph.DrawRectangle(crop_pen, cropX1, cropY1, recW1, recH1);
+                graph.Dispose();
+            }
+        }
+
+        private void Image1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Image1_Click(object sender, EventArgs e)
+        {
+           
+
+
+            
+        }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Image2_MouseUp(object sender, MouseEventArgs e)
+        {
+
+           
+        }
+
+        private void Image2_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Cursor = Cursors.Cross;
+                crop_pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                cropX2 = e.X;
+                cropY2 = e.Y;
+
+            }
+        }
+
+        private void Image2_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Image2.Refresh();
+                recH2 = e.Y - cropY2;
+                recW2 = e.X - cropX2;
+                Graphics graph = Image2.CreateGraphics();
+                graph.DrawRectangle(crop_pen, cropX2, cropY2, recW2, recH2);
+                graph.Dispose();
+            }
+        }
+
+        private void Image2_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void Image2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            int AdminID = Fields.Get_AdminID(Login.username, Login.pword);
+            
+            Fields.Add_Fields(Field1.Image, Field2.Image, Field3.Image, AdminID);
+            MessageBox.Show("Successful!");
+        }
+
+        private void Image1_MouseEnter(object sender, EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Cross;
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            
+        }
+
+        private void Add1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+            Bitmap bmp = new Bitmap(Image1.Width, Image1.Height);
+            Image1.DrawToBitmap(bmp, Image1.ClientRectangle);
+
+            Bitmap crop = new Bitmap(recW1, recH1);
+
+            for (int i = 0; i < recW1; i++)
+            {
+                for (int j = 0; j < recH1; j++)
+                {
+                    Color px = bmp.GetPixel(cropX1 + i, cropY1 + j);
+                    crop.SetPixel(i, j, px);
+
+                }
+            }
+            if (f1 == false)
+            {
+                Field1.Image = (Image)crop;
+                Field1.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                f1 = true;
+            }
+            else if (f2 == false)
+            {
+                Field2.Image = (Image)crop;
+                Field2.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                f2 = true;
+
+            }
+            else { error.Text = "Invalid!"; }
+        }
+
+        private void Add2_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+            Bitmap bmp = new Bitmap(Image2.Width, Image2.Height);
+            Image2.DrawToBitmap(bmp, Image2.ClientRectangle);
+
+            Bitmap crop = new Bitmap(recW2, recH2);
+
+            for (int i = 0; i < recW2; i++)
+            {
+                for (int j = 0; j < recH2; j++)
+                {
+                    Color px = bmp.GetPixel(cropX2 + i, cropY2 + j);
+                    crop.SetPixel(i, j, px);
+
+                }
+            }
+            
+            if (f3 == false)
+            {
+                Field3.Image = (Image)crop;
+                Field3.SizeMode = PictureBoxSizeMode.CenterImage;
+                f3 = true;
+            }
+            else { error.Text = "Invalid!"; }
+        }
+
+        private void Delete_Field1_Click(object sender, EventArgs e)
+        {
+            Field_1.Visible = true;
+            Field_2.Visible = true;
+
+        }
+
+        private void Field_1_Click(object sender, EventArgs e)
+        {
+            Field1.Image.Dispose();
+            Field1.Image = null;
+            f1 = false;
+           
+        }
+
+        private void Field_2_Click(object sender, EventArgs e)
+        {
+            Field2.Image.Dispose();
+            Field2.Image = null;
+            f2 = false;
+        }
+
+        private void Delete_Field2_Click(object sender, EventArgs e)
+        {
+            Field3.Image.Dispose();
+            Field3.Image = null;
+            f3 = false;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you logout?", "Mood Test", MessageBoxButtons.YesNo);
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    {
+                        Login l = new Login();
+                        this.Hide();
+                        l.Show();
+                        break;
+                    }
+                case DialogResult.No:
+                    break;
+            }
+        }
+    
+    }
+}
